2e6a564318b274f78efd23bb09f97c04d63bfc52
jullysilva <jkasilva@sga.pucminas.br>
2023-12-26 19:10:09 -0300

    [CODE]: created structure of project

---
+++ src/pages/Example/Example.tsx
@@ -0,0 +1,10 @@
+import { TextExample } from 'components/TextExample/TextExample'
+import { Container } from './styles'
+
+export const Example = () => {
+  return (
+    <Container>
+      <TextExample>Example</TextExample>
+    </Container>
+  )
+}
