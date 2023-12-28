9a3f67353d983de83019218c2e4c779a8820cdb0
jullysilva <jkasilva@sga.pucminas.br>
2023-12-28 10:51:19 -0300

    [code]: created themes of project

---
+++ src/@types/Themes.ts
@@ -0,0 +1,28 @@
+export default {
+    typograph: {
+        Roboto100: `100 'Roboto', sans-serif;`,
+        Roboto200: `200 'Roboto', sans-serif;`,
+        Roboto300: `300 'Roboto', sans-serif;`,
+        Roboto400: `400 'Roboto', sans-serif;`,
+        Roboto500: `500 'Roboto', sans-serif;`,
+        Roboto600: `600 'Roboto', sans-serif;`,
+        Roboto700: `700 'Roboto', sans-serif;`,
+        Roboto800: `800 'Roboto', sans-serif;`,
+        Roboto900: `900 'Roboto', sans-serif;`,
+    },
+    size: {
+        s8: '8vh',
+        s16: '16vh',
+        s32: '32vh',
+        s64: '64vh'
+    },
+    color: {
+        lightgray: '#E2E2E2',
+        gray: '#808080',
+        blue: 'linear-gradient(to bottom, rgba(81,75,195,0.318)',
+        mossgreen: '#666600',
+        wine: '#660000',
+        lightblack: '#2F2E2E',
+        white: "#FFFFFF"
+    }
+}
